a0d728d4cd1732a81ac750b009677f9608744f4e
PINTO <138750@rba.gov.au>
2022-02-09 12:09:24 +1100

    JWT commit

---
+++ src/main/java/com/pinto/jwt/controller/HomeController.java
@@ -0,0 +1,60 @@
+package com.pinto.jwt.controller;
+
+import com.pinto.jwt.model.JwtRequest;
+import com.pinto.jwt.model.JwtResponse;
+import com.pinto.jwt.service.UserService;
+import com.pinto.jwt.utility.JWTUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private JWTUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String home() {
+        String jsonString = new JSONObject()
+                .put("JSON1", "Hello JWT World!")
+                .toString();
+     return  jsonString;
+
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+
+        final String token =
+                jwtUtility.generateToken(userDetails);
+
+        return  new JwtResponse(token);
+    }
+}
